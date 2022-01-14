a4f90b7f8dce4a8ea64514f9b3ceee8d49fcc484
peter <pjrein@xs4all.nl>
2022-01-14 22:33:50 -0100

    tekst aangepast

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Flask under Apache! herstarten nu in venv '
+    return 'Hello Flask in ubuntu en env '
 
 if __name__ == "__main__":
     app.run()
