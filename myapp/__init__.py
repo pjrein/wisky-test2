2b9937c33e1a2eed677dfb8eb31454e085205466
peter <pjrein@xs4all.nl>
2022-01-14 23:30:42 -0100

    app naar myapp hernoemd en tekst aangepast

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Flask in ubuntu en env '
+    return 'Hello dit moet ook werken op pi in spijkenisse hoop ik '
 
 if __name__ == "__main__":
     app.run()
