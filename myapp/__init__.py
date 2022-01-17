4d11662851ef732ddd5239eb327d46b71b3d51d1
peter <pjrein@xs4all.nl>
2022-01-17 22:42:13 -0100

    uwsgi met dev.ini toegevoegd

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello dit moet ook werken op pi in spijkenisse hoop ik '
+    return 'Hello 9090 dit moet ook werken op pi in spijkenisse hoop ik toch '
 
 if __name__ == "__main__":
     app.run()
--- wsgi-scripts/myapp.wsgi
+++
@@ -1,3 +0,0 @@
-import sys
-sys.path.insert(0, 'D:\\py\wisky-test2')
-from myapp import app as application
